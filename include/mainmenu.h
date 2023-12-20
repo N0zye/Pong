f2738b2bd7b880f3cc228454e7cbd2e9da07f532
N0zye <84083205+N0zye@users.noreply.github.com>
2023-12-20 22:34:18 +0100

    Update mainmenu.h

--- include/mainmenu.h
+++ include/mainmenu.h
@@ -18,7 +18,7 @@ static const GuiStyleProp mainmenuStyleProps[MAINMENU_STYLE_PROPS_COUNT] = {
     { 0, 0, 0xffffffff },    // DEFAULT_BORDER_COLOR_NORMAL 
     { 0, 1, 0x000000ff },    // DEFAULT_BASE_COLOR_NORMAL 
     { 0, 2, 0xffffffff },    // DEFAULT_TEXT_COLOR_NORMAL 
-    { 0, 3, 0xffffffff },    // DEFAULT_BORDER_COLOR_FOCUSED 
+    { 0, 3, 0xb7ffabff },    // DEFAULT_BORDER_COLOR_FOCUSED 
     { 0, 4, 0x000000ff },    // DEFAULT_BASE_COLOR_FOCUSED 
     { 0, 5, 0xffffffff },    // DEFAULT_TEXT_COLOR_FOCUSED 
     { 0, 6, 0x00ff00ff },    // DEFAULT_BORDER_COLOR_PRESSED 
@@ -29,7 +29,7 @@ static const GuiStyleProp mainmenuStyleProps[MAINMENU_STYLE_PROPS_COUNT] = {
     { 0, 19, 0x000000ff },    // DEFAULT_BACKGROUND_COLOR 
     { 0, 20, 0x0000000f },    // DEFAULT_TEXT_LINE_SPACING 
     { 2, 12, 0x00000005 },    // BUTTON_BORDER_WIDTH
-	{ 3, 12, 0x00000005 },    // TOGGLE_BORDER_WIDTH 
+    { 3, 12, 0x00000005 },    // TOGGLE_BORDER_WIDTH 
 };
 
 // Style loading function: mainMenu
